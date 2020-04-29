98a16943fac3c5624479619937ac0379fa2b8b8a
Jeff-Sutherland-Rigor-Guild <59149484+Jeff-Sutherland-Rigor-Guild@users.noreply.github.com>
2020-04-29 12:53:46 +0200

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,6 +4,7 @@ $(function() {
 
     // in a real application, the user/password should never be hardcoded
     // but for the demo application it's very convenient to do so
+    // TODO
     if (!usernameEl.val() || 'jane_admin' === usernameEl.val()) {
         usernameEl.val('jane_admin');
         passwordEl.val('kitten');
